598a37f0698d6eb1ca514a92f6622f7e483e531e
Anika Bernstein <anikajb92@gmail.com>
2021-09-27 15:42:50 -0600

    class method completed is returning number of tasks completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :tasks
 
   validates :username, presence: true, uniqueness: true
+
+  def completed
+    total = self.tasks.count
+    all_completed = self.tasks.where(completed: true).count
+    completed_percentage = ((all_completed.to_f / total.to_f) * 100).to_i
+    pending = total - all_completed
+
+    p "You have completed #{completed_percentage}% of your #{total} tasks, which leaves #{pending} tasks remaining."
+  end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.destroy_all
+User.destroy_all
+
+user1 = User.create(
+  {
+    username: 'MayaPapaya', 
+    password: 'maya1', 
+    firstname: 'Maya', 
+    lastname: 'Papaya',
+  }
+)
+
+Task.create(
+  [
+    {
+      user: user1,
+      description: 'Do dishes', 
+      priority: 1, 
+      completed: false, 
+      category: 'Household'
+    }, 
+    {
+      user: user1,
+      description: 'Meeting with boss', 
+      priority: 3, 
+      completed: true, 
+      category: 'Work'
+    }, 
+    {
+      user: user1,
+      description: 'Get drinks with friends', 
+      priority: 2, 
+      completed: false, 
+      category: 'Social'
+    }, 
+    {
+      user: user1,
+      description: 'Read emails', 
+      priority: 1, 
+      completed: true, 
+      category: 'Work'
+    },
+    {
+      user: user1,
+      description: 'Brush teeth', 
+      priority: 2, 
+      completed: true, 
+      category: 'Personal'
+    }
+  ]
+)
